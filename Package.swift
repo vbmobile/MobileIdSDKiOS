5d4409c60bca477ba3a7f37cf2ef7dfbe1750e07
Pedro Ferreira <pedro.ferreira@vision-box.com>
2025-06-24 09:26:11 +0100

    version 8.1.2

--- Package.swift
+++ Package.swift
@@ -18,15 +18,15 @@ let package = Package(
         .package(url: "https://github.com/airbnb/lottie-spm", .exact("4.4.1")),
         .package(url: "https://github.com/vbmobile/VBDependencyInjector", .exact("1.0.5")),
         .package(url: "https://github.com/vbmobile/VBNetworkClient", .exact("5.1.1")),
-        .package(url: "https://github.com/vbmobile/VBImageProcessor", .exact("1.2.1")),
-        .package(url: "https://github.com/vbmobile/VBUtils", .exact("2.0.1")),
+        .package(url: "https://github.com/vbmobile/VBImageProcessor", .exact("1.2.2")),
+        .package(url: "https://github.com/vbmobile/VBUtils", .exact("2.0.2")),
         .package(url: "https://github.com/vbmobile/matomo-sdk-ios", .exact("7.7.0")),
     ],
     targets: [
         .binaryTarget(
             name: "MobileIdSDKiOS",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/MobileIdSDKiOS/MobileIdSDKiOS/MobileIdSDKiOS-8.1.1.zip",
-            checksum: "f43a1524d8cbbebe8952ff14e4edf6c91e091e6b33dc659ad2190ed7ed72c7f4"
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/MobileIdSDKiOS/MobileIdSDKiOS/MobileIdSDKiOS-8.1.2.zip",
+            checksum: "b63b018c6a6c1de91414ba86940260695154aa618623296b2d939c1ec8294bc4"
         ),
         .target(
             name: "MobileIdSDKiOSWrapper",
